39e1dd06cd8c6220a17aad01905ec2feed702b81
zhangmn <Zhang1880!>
2019-12-05 18:25:25 +0800

    控制器拦截异常转发到特定的页面

---
+++ src/main/java/com/itheima/expetion/ControllerExceptionAdvice.java
@@ -0,0 +1,19 @@
+package com.itheima.expetion;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ControllerExceptionAdvice {
+    //只有出现AccessDeniedException异常才调转403.jsp页面
+    @ExceptionHandler(AccessDeniedException.class)
+    public String exceptionAdvice() {
+        return "forward:/403.jsp";
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public String runtimeexceptionAdvice() {
+        return "forward:/500.jsp";
+    }
+}
