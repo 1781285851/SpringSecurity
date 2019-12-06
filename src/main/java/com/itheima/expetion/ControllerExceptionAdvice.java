9ade527a098c1d810fbe534e277c405629f3b193
zhangmn <Zhang1880!>
2019-12-06 11:00:26 +0800

    修改页面出现异常无法跳转的原因，之前没有扫描异常包

--- src/main/java/com/itheima/expetion/ControllerExceptionAdvice.java
+++ src/main/java/com/itheima/expetion/ControllerExceptionAdvice.java
@@ -4,16 +4,24 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+/**
+ * 这种方式无法处理404异常
+ */
 @ControllerAdvice
 public class ControllerExceptionAdvice {
     //只有出现AccessDeniedException异常才调转403.jsp页面
     @ExceptionHandler(AccessDeniedException.class)
     public String exceptionAdvice() {
-        return "forward:/403.jsp";
+        System.out.println("出现403");
+        return "redirect:/403.jsp";
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public String runtimeexceptionAdvice() {
-        return "forward:/500.jsp";
+        public String runtimeExceptionAdvice() {
+        System.out.println("出现500");
+        return "redirect:/500.jsp";
     }
+
+
+
 }
